bf6af9049a7ec3cbca9163907d714f2f332376af
Eshan05 <nahareshan05@gmail.com>
2024-08-28 21:26:56 +0530

    Functionality of adding organizations
    - Styled plus button in sidebar
    - Added shadcn components
    - Added clerk CreateOrganization
    - Added list.tsx

--- app/(dashboard)/_components/Sidebar/index.tsx
+++ app/(dashboard)/_components/Sidebar/index.tsx
@@ -1,7 +1,11 @@
+import { List } from "./list";
+import { NewButton } from "./new-button";
+
 export const Sidebar = () => {
   return (
-    <div>
-      Sidebar!
-    </div>
+    <aside className="fixed z-[1] left-0 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white bg-slate-400">
+      <List />
+      <NewButton />
+    </aside>
   );
 };
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import { NavBar } from "./_components/navbar";
+import { OrgSidebar } from "./_components/org-sidebar";
 import { Sidebar } from "./_components/Sidebar"
 interface DashboardLayoutProps {
   children: React.ReactNode
@@ -10,7 +12,13 @@ const DashboardLayout = ({
     <main className="h-full">
       <Sidebar />
       <div className="h-full pl-[16px]">
-      {children}
+        <div className="flex gap-x-3 h-full">
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <NavBar />
+            {children}
+          </div>
+        </div>
       </div>
     </main>
   );
