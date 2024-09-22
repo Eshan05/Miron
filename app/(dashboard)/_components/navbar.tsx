b04cf682fbc29592d68ad347351dcae3623a818e
Eshan05 <nahareshan05@gmail.com>
2024-09-22 21:00:33 +0530

    Basic search implementation

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,14 +1,15 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { SearchInput } from "./search-input";
 
 export const NavBar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+    <div className="flex items-center gap-x-4 p-5">
       <div className="hidden lg:flex lg:flex-1">
-        {/* TODO: Add search*/}
+        <SearchInput />
       </div>
-        <UserButton />
+      <UserButton />
     </div>
   )
 }
