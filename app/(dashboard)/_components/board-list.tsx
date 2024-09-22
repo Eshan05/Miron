c5f0e68914cc92899d65df14a157f68471f9d86e
Eshan05 <nahareshan05@gmail.com>
2024-09-22 21:58:51 +0530

    Add conditional components in board-list

---
+++ app/(dashboard)/_components/board-list.tsx
@@ -0,0 +1,43 @@
+"use client"
+
+import { EmptyFavorites } from "./empty-favorites";
+import { EmptySearch } from "./empty-search";
+
+interface BoardListProps {
+  orgId: string
+  query: {
+    search?: string
+    favorites?: string
+  };
+};
+
+export const BoardList = ({
+  orgId,
+  query,
+}: BoardListProps) => {
+  const data = [] // TODO: Change to API call
+  if (!data?.length && query.search) {
+    return (
+      <EmptySearch />
+    )
+  }
+
+  if (!data?.length && query.favorites) {
+    return (
+      <EmptyFavorites />
+    );
+  }
+
+  if (!data?.length) {
+    return (
+      <div>
+        No boards
+      </div>
+    )
+  }
+  return (
+    <div>
+      {JSON.stringify(query)}
+    </div>
+  )
+}
