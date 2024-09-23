e023cb4610cece76a904687595456b0a2da666fb
Eshan05 <nahareshan05@gmail.com>
2024-09-23 17:38:10 +0530

    Add superficial functionality for boards creation
    - Create schema for boards
    - Add functionality to store board in Convex DB on clicking create board in empty-board

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,6 +1,20 @@
+"use client"
+import { api } from "@/convex/_generated/api";
+import { useMutation } from "convex/react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import { useOrganization } from "@clerk/nextjs";
+import { title } from "process";
 export const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const create = useMutation(api.board.create);
+  const onClick = () => {
+    if (!organization) return;
+    create({
+      orgId: organization.id,
+      title: "Untitled",
+    })
+  }
   return (
     <div className="h-full flex flex-col items-center justify-center">
       <Image
@@ -10,7 +24,7 @@ export const EmptyBoards = () => {
         width={110} /><h2 className="text-2xl font-semibold mt-6">Create your first board</h2>
       <p className="text-sm mt-2 text-muted-foreground"> Start by creating a board for your organization</p>
       <div className="mt-6">
-        <Button size={"lg"}>Create board</Button>
+        <Button size={"lg"} onClick={onClick}>Create board</Button>
       </div>
     </div>
   )
