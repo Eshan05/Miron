597c0f3e8db5ab51a9033b37b9f05baf87db4479
Eshan05 <nahareshan05@gmail.com>
2024-09-23 17:49:43 +0530

    Make custom hook useApiMutation

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,16 +1,16 @@
 "use client"
 import { api } from "@/convex/_generated/api";
-import { useMutation } from "convex/react";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import { useOrganization } from "@clerk/nextjs";
 import { title } from "process";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 export const EmptyBoards = () => {
   const { organization } = useOrganization();
-  const create = useMutation(api.board.create);
+  const { mutate, pending } = useApiMutation(api.board.create);
   const onClick = () => {
     if (!organization) return;
-    create({
+    mutate({
       orgId: organization.id,
       title: "Untitled",
     })
@@ -24,7 +24,7 @@ export const EmptyBoards = () => {
         width={110} /><h2 className="text-2xl font-semibold mt-6">Create your first board</h2>
       <p className="text-sm mt-2 text-muted-foreground"> Start by creating a board for your organization</p>
       <div className="mt-6">
-        <Button size={"lg"} onClick={onClick}>Create board</Button>
+        <Button disabled={pending} size={"lg"} onClick={onClick}>Create board</Button>
       </div>
     </div>
   )
