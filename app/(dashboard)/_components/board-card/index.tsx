649d1b4eb7dfc1ef20046950d51820cf356bf0f4
Eshan05 <nahareshan05@gmail.com>
2024-09-24 04:22:20 +0530

    Implement Board Cards

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,27 @@
+"use client"
+
+interface BoardCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  authorId: string;
+  authorName: string;
+  createdAt: number;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+export const BoardCard = ({
+  id,
+  title,
+  authorId,
+  authorName,
+  createdAt,
+  imageUrl,
+  orgId,
+  isFavorite
+}: BoardCardProps) => {
+  return (
+    <div>Board card</div>
+  )
+}
