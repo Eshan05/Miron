7d7b552b08bd95a97a66a4f5a8340dcde8c4a022
Eshan05 <nahareshan05@gmail.com>
2024-09-24 04:56:05 +0530

    Added onClick functionality  for new-board-button

--- app/(dashboard)/_components/new-board-button.tsx
+++ app/(dashboard)/_components/new-board-button.tsx
@@ -1,7 +1,8 @@
 "use client";
+import { useApiMutation } from "@/hooks/use-api-mutation";
 import { cn } from "@/lib/utils";
 import { Plus } from "lucide-react";
-
+import { api } from "@/convex/_generated/api";
 
 interface NewBoardButtonProps {
   disabled?: boolean;
@@ -12,16 +13,24 @@ export const NewBoardButton = ({
   orgId,
   disabled,
 }: NewBoardButtonProps) => {
+  const { mutate, pending } = useApiMutation(api.board.create);
+  const onClick = () => {
+    mutate({
+      orgId,
+      title: "Untitled",
+    })
+  }
   return (
     <button
-      disabled={disabled}
-      onClick={() => { }}
+      disabled={pending || disabled}
+      onClick={onClick}
       className={cn(
-        "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6"
+        "col-span-1 aspect-[100/127] bg-blue-600 rounded-lg hover:bg-blue-800 flex flex-col items-center justify-center py-6",
+        (pending || disabled) && "cursor-not-allowed opacity-75"
       )}
     >
       <Plus className="h-12 text-white w-12 stroke-1" />
-      <p className="text-sm text-white font-light">New Board</p>
+      <p className="text-xs pt-4 text-white font-light">New Board</p>
     </button>
   )
 };
