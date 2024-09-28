97e8b95abd980c565fa59c5f5c0075f703fd67a4
Eshan05 <nahareshan05@gmail.com>
2024-09-28 23:01:24 +0530

    Checkpoint for tool selection

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,8 +1,11 @@
 "use client";
 
+import { useState } from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
+import { CanvasMode, CanvasState } from "@/types/canvas";
+import { useCanRedo, useCanUndo, useHistory, useRedo } from "@/liveblocks.config";
 
 interface CanvasProps {
   boardId: string
@@ -11,11 +14,21 @@ interface CanvasProps {
 export const Canvas = ({ boardId }: CanvasProps) => {
   // const info = useSelf((me) => me.info);
   // console.log(info)
+  const [canvasState, setCanvasState] = useState<CanvasState>({ mode: CanvasMode.None, })
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
   return (
     <main className="h-[100svh] w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={false}
+        canUndo={false}
+        undo={history.undo}
+        redo={history.redo} />
     </main>
   );
 }
--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,11 +1,28 @@
 import { Circle, MousePointer2, Pen, Redo2, Square, StickyNoteIcon, Type, Undo2 } from "lucide-react";
 import { ToolButton } from "./tool-button";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 
-export const Toolbar = () => {
+type ToolbarProps = {
+  canvasState: CanvasState;
+  setCanvasState: (newState: CanvasState) => void;
+  undo: () => void;
+  redo: () => void;
+  canUndo: boolean;
+  canRedo: boolean;
+};
+
+export const Toolbar = ({
+  canvasState,
+  setCanvasState,
+  undo,
+  redo,
+  canUndo,
+  canRedo,
+}: ToolbarProps) => {
   return (
     <div className="absolute top-1/2 -translate-y-1/2 left-2 flex flex-col gap-y-4">
       <div className="bg-white rounded-md p-1.5 flex gap-y-1 flex-col items-center shadow-md">
-        <ToolButton label="Select" icon={MousePointer2} onClick={() => { }} isActive={false} />
+        <ToolButton label="Select" icon={MousePointer2} onClick={() => setCanvasState({ mode: CanvasMode.None })} isActive={canvasState.mode === CanvasMode.None} />
         <ToolButton label="Type" icon={Type} onClick={() => { }} isActive={false} />
         <ToolButton label="Sticky Note" icon={StickyNoteIcon} onClick={() => { }} isActive={false} />
         <ToolButton label="Rectangle" icon={Square} onClick={() => { }} isActive={false} />
@@ -13,8 +30,8 @@ export const Toolbar = () => {
         <ToolButton label="Pen" icon={Pen} onClick={() => { }} isActive={false} />
       </div>
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
-        <ToolButton label="Undo" icon={Undo2} onClick={() => { }} isDisabled={false} />
-        <ToolButton label="Redo" icon={Redo2} onClick={() => { }} isDisabled={false} />
+        <ToolButton label="Undo" icon={Undo2} onClick={undo} isDisabled={!canUndo} />
+        <ToolButton label="Redo" icon={Redo2} onClick={redo} isDisabled={!canRedo} />
       </div>
     </div>
   );
