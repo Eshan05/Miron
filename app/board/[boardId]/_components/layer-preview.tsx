0f58ec81cd5035af932804c9d1074e6b944e1642
Eshan05 <nahareshan05@gmail.com>
2024-09-29 10:43:18 +0530

    Implmentation of basic Rectangle layer functionality via toolbutton
    - Add new component rectangle
    - QoL for layout.tsx
    - Layer definitions in canvas.tsx

---
+++ app/board/[boardId]/_components/layer-preview.tsx
@@ -0,0 +1,33 @@
+"use client"
+
+import { useStorage } from "@/liveblocks.config";
+import { LayerType } from "@/types/canvas";
+import { memo } from "react";
+import { Rectangle } from "./rectangle";
+
+type LayerPreviewProps = {
+  id: string;
+  onLayerPointerDown: (e: React.PointerEvent, id: string) => void;
+  selectionColor?: string;
+};
+
+export const LayerPreview = memo(
+  ({ id, onLayerPointerDown, selectionColor }: LayerPreviewProps) => {
+    const layer = useStorage((root) => root.layers.get(id))
+    if (!layer) return null;
+
+    switch (layer.type) {
+      case LayerType.Rectangle:
+        return (
+          <Rectangle
+            id={id}
+            layer={layer}
+            onPointerDown={onLayerPointerDown}
+            selectionColor={selectionColor}
+          />
+        );
+      default: console.warn("Unknown layer type", layer); return null;
+    };
+  });
+
+LayerPreview.displayName = "LayerPreview"
