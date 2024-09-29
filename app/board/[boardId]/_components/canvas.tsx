ad9e4f751c94a48590f09f3e9fd3a76fb053a13b
Eshan05 <nahareshan05@gmail.com>
2024-09-29 10:01:12 +0530

    Make onPointerUp in canvas.tsx
    - Confirmation of Undo and Redo functionality
    - Confirmation of coordinates mapping and default to select
    - Add new things in liveblocks.config.ts
    - Add nanoid package

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -1,27 +1,64 @@
 "use client";
 
+import { nanoid } from "nanoid";
 import { useCallback, useState } from "react";
 import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
-import { Camera, CanvasMode, CanvasState } from "@/types/canvas";
-import { useCanRedo, useCanUndo, useHistory, useRedo, useMutation } from "@/liveblocks.config";
+import { Camera, CanvasMode, CanvasState, Color, LayerType, Point } from "@/types/canvas";
+import { useCanRedo, useCanUndo, useHistory, useRedo, useMutation, useStorage } from "@/liveblocks.config";
 import { CursorsPresence } from "./cursors-presence";
 import { pointerEventToCanvasPoint } from "@/lib/utils";
+import { LiveObject } from "@liveblocks/client";
+
+const MAX_LAYERS = 100;
 
 interface CanvasProps {
   boardId: string
 }
 
 export const Canvas = ({ boardId }: CanvasProps) => {
+  const layerIds = useStorage((root) => root.layerIds)
   // const info = useSelf((me) => me.info);
   // console.log(info)
   const [canvasState, setCanvasState] = useState<CanvasState>({ mode: CanvasMode.None, })
   const [camera, setCamera] = useState<Camera>({ x: 0, y: 0 })
+  const [lastUsedColor, setLastUsedColor] = useState<Color>({
+    r: 0,
+    g: 0,
+    b: 0,
+  })
 
   const history = useHistory();
   const canUndo = useCanUndo();
   const canRedo = useCanRedo();
+
+  const insertLayer = useMutation((
+    { storage, setMyPresence },
+    layerType: LayerType.Ellipse | LayerType.Rectangle | LayerType.Text | LayerType.Note,
+    position: Point,
+  ) => {
+    const liveLayers = storage.get("layers");
+    if (liveLayers.size >= MAX_LAYERS) {
+      return;
+    }
+
+    const liveLayersIds = storage.get("layerIds");
+    // Package nanoid install
+    const layerId = nanoid();
+    const layer = new LiveObject({
+      type: layerType,
+      x: position.x,
+      y: position.y,
+      height: 100,
+      width: 100,
+      fill: lastUsedColor
+    });
+    liveLayersIds.push(layerId);
+    liveLayers.set(layerId, layer);
+    setMyPresence({ selection: [layerId] }, { addToHistory: true });
+    setCanvasState({ mode: CanvasMode.None });
+  }, [lastUsedColor])
 
   const onWheel = useCallback((e: React.WheelEvent) => {
     setCamera((camera) => ({
@@ -33,9 +70,32 @@ export const Canvas = ({ boardId }: CanvasProps) => {
   const onPointerMove = useMutation(({ setMyPresence }, e: React.PointerEvent) => {
     e.preventDefault();
     const current = pointerEventToCanvasPoint(e, camera);
-    console.log({ current })
+    // console.log({ current })
     setMyPresence({ cursor: current })
   }, [])
+
+  const onPointerLeave = useMutation(({ setMyPresence }) => {
+    setMyPresence({
+      cursor: null
+    })
+  }, [])
+
+  const onPointerUp = useMutation(({ }, e) => {
+    const point = pointerEventToCanvasPoint(e, camera);
+    console.log({ point, mode: canvasState.mode });
+    if (canvasState.mode === CanvasMode.Inserting) {
+      insertLayer(canvasState.layerType, point);
+    } else {
+      mode: (CanvasMode.None);
+    }
+
+    history.resume();
+  }, [
+    camera,
+    canvasState,
+    history,
+    insertLayer
+  ])
   return (
     <main className="h-[100svh] w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
@@ -51,8 +111,13 @@ export const Canvas = ({ boardId }: CanvasProps) => {
         className="h-[100vh] w-[100vw]"
         onWheel={onWheel}
         onPointerMove={onPointerMove}
+        onPointerLeave={onPointerLeave}
+        onPointerUp={onPointerUp}
       >
-        <g>
+        <g
+          style={{
+            transform: `translate(${camera.x}px ${camera.y}px)`,
+          }}>
           <CursorsPresence />
         </g>
       </svg>
